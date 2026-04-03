ef54c9351f7138ad0d7736fd22223cf08666ff4a
Oliver Haas <ohaas@e1plus.de>
2026-04-03 22:38:49 +0200

    feat: remove @template_tag decorator and BaseNode public export
    Remove the public API for creating custom template tags using
    django-components' BaseNode infrastructure. BaseNode is still
    used internally but no longer exported.
    
    Also removed test_html_parser.py (tested removed dependency system)
    and fixed pytest addopts to not require pytest-cov.
    
    Test results: 362 passed, 3 skipped, ~7s

---
+++ django_components/__init__.py
@@ -0,0 +1,85 @@
+"""Main package for Django Components."""
+
+# Public API
+# isort: off
+from django_components.app_settings import ComponentsSettings
+from django_components.attributes import format_attributes, merge_attributes
+from django_components.autodiscovery import autodiscover, import_libraries
+from django_components.component import (
+    Component,
+    ComponentInput,
+    ComponentNode,
+    ComponentVars,
+    all_components,
+    get_component_by_class_id,
+)
+from django_components.component_registry import (
+    AlreadyRegistered,
+    ComponentRegistry,
+    NotRegistered,
+    RegistrySettings,
+    register,
+    registry,
+    all_registries,
+)
+from django_components.dependencies import DependenciesStrategy, render_dependencies
+from django_components.library import TagProtectedError
+from django_components.slots import (
+    FillNode,
+    Slot,
+    SlotContent,
+    SlotContext,
+    SlotFallback,
+    SlotFunc,
+    SlotInput,
+    SlotNode,
+    SlotRef,
+    SlotResult,
+)
+from django_components.template import cached_template
+import django_components.types as types  # noqa: PLR0402
+from django_components.util.loader import ComponentFileEntry, get_component_dirs, get_component_files
+from django_components.util.types import Empty
+
+# isort: on
+
+
+__all__ = [
+    "AlreadyRegistered",
+    "Component",
+    "ComponentFileEntry",
+    "ComponentInput",
+    "ComponentNode",
+    "ComponentRegistry",
+    "ComponentVars",
+    "ComponentsSettings",
+    "DependenciesStrategy",
+    "Empty",
+    "FillNode",
+    "NotRegistered",
+    "RegistrySettings",
+    "Slot",
+    "SlotContent",
+    "SlotContext",
+    "SlotFallback",
+    "SlotFunc",
+    "SlotInput",
+    "SlotNode",
+    "SlotRef",
+    "SlotResult",
+    "TagProtectedError",
+    "all_components",
+    "all_registries",
+    "autodiscover",
+    "cached_template",
+    "format_attributes",
+    "get_component_by_class_id",
+    "get_component_dirs",
+    "get_component_files",
+    "import_libraries",
+    "merge_attributes",
+    "register",
+    "registry",
+    "render_dependencies",
+    "types",
+]
